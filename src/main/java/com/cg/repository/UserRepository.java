f7367dbb3a60b4ca19ebde8ad07c07d5875ccd2f
Rounack Kundu <rounack1@gmail.com>
2021-10-07 03:47:29 +0530

    Repository Added

--- src/main/java/com/cg/repository/UserRepository.java
+++ src/main/java/com/cg/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.cg.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cg.model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 }
