5579c6e144af60ee8f8846bfe1ba84bfbc94119d
SoumenduMaitra <soumendu.maitra1998@gmail.com>
2021-10-09 01:44:49 +0530

    bsbas

---
+++ src/main/java/com/cg/repository/LoginRepository.java
@@ -0,0 +1,9 @@
+package com.cg.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cg.model.Login;
+
+public interface LoginRepository extends JpaRepository<Login, Integer> {
+
+}
