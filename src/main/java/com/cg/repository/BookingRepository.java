c610c37b487d5a50dbe0476baafd72191d908f5e
titas1803 <titas.sarkar420@hotmail.com>
2021-10-10 16:31:51 +0530

    blank

--- src/main/java/com/cg/repository/BookingRepository.java
+++ src/main/java/com/cg/repository/BookingRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cg.model.BookingDetails;
+
 @Repository
 public interface BookingRepository extends JpaRepository<BookingDetails, Integer> {
 
--- src/main/java/com/cg/repository/LoginRepository.java
+++ src/main/java/com/cg/repository/LoginRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.cg.model.Login;
+
 @Repository
 public interface LoginRepository extends JpaRepository<Login, Integer> {
 
